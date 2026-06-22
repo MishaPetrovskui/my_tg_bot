25d9099830d5fc8ed5bbf51e81f457d1de130390
student <student@L02-19>
2026-06-22 19:00:29 +0300

    fix

--- folder/main.py
+++ folder/main.py
@@ -15,23 +15,16 @@ from google.genai import types
 from PromptBuilder import PromptBuilder
 
 
-dp = Dispatcher()                        # [2]
+dp = Dispatcher()
 client = None
 bot = None
-current_message_context = None
 
-BASE_DIR = "/home/ec2-user/tg_bot/folder"
-
-
-# Підключення до telegram-бота
 def auth_telegram():
-    token = getenv("BOT_TOKEN")  # [7]
-    if not token:  # [7]
-        error = "No token provided"  # [7]
-        raise ValueError(error)  # [7]
-    return Bot(token=token)  # [8]
+    token = getenv("BOT_TOKEN")
+    if not token:
+        raise ValueError("No token provided")
+    return Bot(token=token)
 
-# Підключення Gemini API
 def auth_gemini_api():
     api_key = getenv("GEMINI_API_KEY")
     if not api_key:
@@ -43,57 +36,50 @@ def auth_gemini_api():
         print("Can`t connect to Gemini API. Running without one.")
     return None
 
-import random
-
 @dp.message(Command("roll"))
 async def cmd_roll(message: Message):
     result = random.randint(1, 6)
     await message.answer(f"🎲 {result}")
 
-# Обробник команди /start
 @dp.message(Command("start"))
 async def cmd_start(message: Message):
     await message.answer("Let`s talk!")
 
-# Обробних всіх інших повідомлень
-@dp.message()                            # [3]
-async def any_message(                   # [4]
-        message: Message,                # [5]
-):
+@dp.message()
+async def any_message(message: Message):
     print(f"{message.from_user.full_name}: {message.text}")
+    
     if client is None:
-        await message.answer("Hello world!")
+        await message.answer("Hello world! (Gemini API не подключен)")
     else:
         try:
             prompt = PromptBuilder.simple_prompt(message.text)
-            await message.answer(f"DEBUG: {prompt}")
+            
             response = client.models.generate_content(
                 model="gemini-3.5-flash",
                 contents=prompt,
             )
+            
+            response_text = str(response.text)
+            await message.answer(response_text)
+            
         except Exception as err:
             import traceback
-
             traceback.print_exc()
-
-            await message.answer(
-                f"Ошибка:\n{type(err).__name__}\n{err}"
-            )
-        response_text = str(response.text)
+            await message.answer(f"Ошибка:\n{type(err).__name__}\n{err}")
 
 async def main():
     global bot, client
-
     load_dotenv()
+    
     bot = auth_telegram()
     client = auth_gemini_api()
 
     print("Starting bot...")
     try:
-        await dp.start_polling(bot)      # [9]
+        await dp.start_polling(bot)
     finally:
         print("Bot stopped")
-
 
 if __name__ == '__main__':
     asyncio.run(main())
